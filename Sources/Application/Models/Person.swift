3b09b8ee5d31081dd09524170dd7787346c8b855
David Jones <djones6@uk.ibm.com>
2018-09-24 10:24:53 +0100

    Add Validatable and ValidatableAsync prototype

--- Sources/Application/Models/Person.swift
+++ Sources/Application/Models/Person.swift
@@ -1,6 +1,20 @@
 import SwiftKueryORM
 
-struct Person: Model {
+struct Person: Model, Validatable, ValidatableAsync {
     let name: String
     let age: Int
+
+    func validate(completion: @escaping (ValidationError?) -> Void) {
+        guard !name.isEmpty else {
+            return completion(ValidationError("Name cannot be empty"))
+        }
+        completion(nil)
+    }
+
+    func validate() throws {
+        guard !name.isEmpty else {
+            throw ValidationError("Name cannot be empty")
+        }
+    }
+
 }
---
+++ Sources/Application/Models/Validatable.swift
@@ -0,0 +1,14 @@
+public protocol Validatable {
+    func validate() throws
+}
+
+public protocol ValidatableAsync {
+    func validate(completion: @escaping (ValidationError?) -> Void)
+}
+
+public struct ValidationError: Error {
+    let reason: String
+    public init(_ reason: String) {
+        self.reason = reason
+    }
+}
