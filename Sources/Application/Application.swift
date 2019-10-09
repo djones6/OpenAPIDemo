c8d9e513a511559f9217d9d1e477757173297ccf
David Jones <djones6@uk.ibm.com>
2019-10-09 13:50:02 +0100

    Update to swift 5.1 and Kuery 3

--- Sources/Application/Application.swift
+++ Sources/Application/Application.swift
@@ -14,7 +14,7 @@ public let health = Health()
 
 class Persistence {
     static func setUp() {
-        let pool = PostgreSQLConnection.createPool(host: "localhost", port: 5432, options: [.databaseName("test")], poolOptions: ConnectionPoolOptions(initialCapacity: 10, maxCapacity: 50, timeout: 10000))
+        let pool = PostgreSQLConnection.createPool(host: "localhost", port: 5432, options: [.databaseName("test")], poolOptions: ConnectionPoolOptions(initialCapacity: 10, maxCapacity: 50))
         Database.default = Database(pool)
     }
 }
